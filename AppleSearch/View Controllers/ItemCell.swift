4efb4cbed8e16cc25422626a84448b3a6d501ca7
Madison Kaori Shino <kaorishino@Madisons-MacBook-Pro.local>
2019-06-27 10:28:50 -0600

    Storyboard View Set Up

---
+++ AppleSearch/View Controllers/ItemCell.swift
@@ -0,0 +1,21 @@
+//
+//  ItemCell.swift
+//  AppleSearch
+//
+//  Created by Madison Kaori Shino on 6/27/19.
+//  Copyright © 2019 Madi S. All rights reserved.
+//
+
+import UIKit
+
+class ItemCell: UITableViewCell {
+
+    //OUTLETS
+    @IBOutlet weak var itemImage: UIImageView!
+    @IBOutlet weak var itemSongName: UILabel!
+    @IBOutlet weak var itemArtistName: UILabel!
+    @IBOutlet weak var itemAlbumName: UILabel!
+    
+    
+
+}
